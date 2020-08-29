d066f728550cd94adbcd579ac8867cc6baeaa02a
Ilya Daraseliya <idarase@gmail.com>
2020-08-30 00:25:15 +0200

    feat: article published state

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    ctx.query = {
+      ...ctx.query,
+      status: "published",
+    };
+
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+    console.log(entities);
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.article })
+    );
+  },
+};
--- api/site/controllers/site.js
+++ api/site/controllers/site.js
@@ -1,8 +1,46 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.site.search(ctx.query).models;
+    } else {
+      entities = await strapi.services.site.find(ctx.query);
+    }
+
+    return entities
+      .map((entity) => {
+        entity = {
+          ...entity,
+          articles: entity.articles.filter(
+            (article) => article.status === "published"
+          ),
+        };
+        return entity;
+      })
+      .map((entity) => sanitizeEntity(entity, { model: strapi.models.site }));
+  },
+  async findOne(ctx) {
+    let entity;
+
+    entity = await strapi.services.site.findOne({ id: ctx.params.id });
+
+    console.log(entity);
+
+    return sanitizeEntity(
+      {
+        ...entity,
+        articles: [
+          ...entity.articles.filter(
+            (article) => article.status === "published"
+          ),
+        ],
+      },
+      { model: strapi.models.site }
+    );
+  },
+};
